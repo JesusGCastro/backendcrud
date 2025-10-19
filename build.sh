8184d1530827612ca880f6e83ea03dab69e23743
JesusGCastro <renecast8@gmail.com>
2025-10-19 14:06:00 -0700

    Mod a build

--- build.sh
+++ build.sh
@@ -8,14 +8,18 @@ pip install -r requirements.txt
 python manage.py migrate
 
 # Crear superusuario automáticamente (solo si no existe)
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-import os; \
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME'); \
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL'); \
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD'); \
-if username and not User.objects.filter(username=username).exists(): \
-    User.objects.create_superuser(username=username, email=email, password=password)" | python manage.py shell
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+END
 
 # Recoger archivos estáticos
 python manage.py collectstatic --noinput
